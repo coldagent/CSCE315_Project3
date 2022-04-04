d35d1bef2cb85ab85aa140830da18ad2fd251e13
Erik Priest <epriest@tamu.edu>
2022-04-04 09:41:44 -0500

    Update app.py
    Added GetRoute and LocToGeoCoords to the context of the jinja2 template

--- app.py
+++ app.py
@@ -1,30 +1,7 @@
 import requests
 from flask import Flask, render_template, request
+from jinja2 import Environment
 app = Flask(__name__)
-
-
-"""
-   Parameters: 
-
-      startCoord: [float, float] | coordinates for start location in form [longitude, latitude]
-      endCoord: [float, float]   | coordinates for start location in form [longitude, latitude]
-
-   returns array of waypoint objects (refer to https://docs.mapbox.com/api/navigation/directions/#waypoint-object for waypoint objects)
-"""
-# def GetRoute(startCoord, endCoord):
-#    # Setup request
-#    websiteSource = "https://api.mapbox.com/directions/v5/mapbox"
-#    travelMethod = "driving"
-#    accessToken = ""
-#    htmlRequest = "{}/{}/{};{}?geometries=geojson&access_token={}".format(websiteSource, travelMethod, ",".join(str(x) for x in startCoord), ",".join(str(x) for x in endCoord), accessToken)
-
-#    # Process response
-#    response = requests.get(htmlRequest)
-
-#    # Should make a function here that directs to an error page for if status_code != 200 (indicates error but 200 can also represent potenetial issues)
-#       #print(response.status_code)
-#    return response.get("waypoints")
-
 
 
 """
@@ -36,32 +13,58 @@ app = Flask(__name__)
 
    returns [float, float] the coordinates of the most relevant location
 """
-# def LocToGeoCoords(location):
-#    # Setup request
-#    websiteSource = "https://api.mapbox.com/geocoding/v5"
-#    endPoint = "mapbox.places"
-#    accessToken = "" 
-#    htmlRequest = "{}/{}/{}.json?access_token={}".format(websiteSource, endPoint, str(location).replace(' ','+').replace(',', "%2C"), accessToken)
+@app.context_processor
+def Utility_Processor():
+   def LocToGeoCoords(location):
+      # Setup request
+      websiteSource = "https://api.mapbox.com/geocoding/v5"
+      endPoint = "mapbox.places"
+      accessToken = "sk.eyJ1Ijoic2lyZXNxdWlyZWdvYXQiLCJhIjoiY2wxZnIxczI0MDZtaTNpbjl0bGlzYjZibyJ9.a-nfmMP6FruqBlSzx9uQOg" 
+      htmlRequest = "{}/{}/{}.json?access_token={}".format(websiteSource, endPoint, str(location).replace(' ','+').replace(',', "%2C"), accessToken)
 
-#    # Process response
-#    response = requests.get(htmlRequest)
-#    results = response.json()
-#    features = results.get("features")
-#    coordinates = features[0].get("geometry").get("coordinates")
-#    print(coordinates)
-#    return coordinates
+      # Process response
+      response = requests.get(htmlRequest)
+      results = response.json()
+      features = results.get("features")
+      coordinates = features[0].get("geometry").get("coordinates")
+      print(coordinates)
+      return coordinates
 
+   """
+   Parameters: 
+
+      startCoord: [float, float] | coordinates for start location in form [longitude, latitude]
+      endCoord: [float, float]   | coordinates for start location in form [longitude, latitude]
+
+      returns array of waypoint objects (refer to https://docs.mapbox.com/api/navigation/directions/#waypoint-object for waypoint objects)
+   """
+   def GetRoute(startCoord, endCoord):
+      # Setup request
+      websiteSource = "https://api.mapbox.com/directions/v5/mapbox"
+      travelMethod = "driving"
+      accessToken = "sk.eyJ1Ijoic2lyZXNxdWlyZWdvYXQiLCJhIjoiY2wxZnIxczI0MDZtaTNpbjl0bGlzYjZibyJ9.a-nfmMP6FruqBlSzx9uQOg"
+      htmlRequest = "{}/{}/{};{}?geometries=geojson&access_token={}".format(websiteSource, travelMethod, ",".join(str(x) for x in startCoord), ",".join(str(x) for x in endCoord), accessToken)
+
+      # Process response
+      response = requests.get(htmlRequest).json()
+
+      # Should make a function here that directs to an error page for if status_code != 200 (indicates error but 200 can also represent potenetial issues)
+         #print(response.status_code)
+      return response.get("waypoints")
+   return dict(LocToGeoCoords = LocToGeoCoords, GetRoute = GetRoute)
+
+
+
+
+
+@app.context_processor
+def SubmitRouteQuery():
+   print("submitted")
+   return dict(SubmitRouteQuery = "submitted")
 
 
 @app.route('/')
 def page1():
-   # Dummy coords until we get a way to read user inputs workflow should be get start (str), end (str) then do GetRoute( LocToGeoCoords(start) , LocToGeoCoords(end) ) 
-   startCoord = [-96.325851, 30.622370] # College Station
-   endCoord = [-95.358421, 29.749907] # Houston
-
-   # GetRoute(startCoord, endCoord)
-   # LocToGeoCoords("College Station, Texas")
-
    return render_template('page1.html')
 
 
