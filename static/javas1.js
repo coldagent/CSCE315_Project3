97e5836d2767e24fc1c7ce2d3f13b00e5241ae13
Erik Priest <epriest@tamu.edu>
2022-04-06 20:04:58 -0500

    Update javas1.js
    Fixed cookies updating out of sync

--- static/javas1.js
+++ static/javas1.js
@@ -1,29 +1,45 @@
 
+var firstCookieDone = false;
+var secondCookieDone = false;
+
 // When "pg1-submit" button is clicked, 
 //start and end coords are saved to cookie
 document.getElementById("pg1-submit").addEventListener("click", function(e) {
     let start = document.getElementById("text1").value;
     let end = document.getElementById("text2").value;
 
+    FetchCoordinates(start, end);
+    CheckIfDone();
+});
 
+
+function FetchCoordinates(start, end){
     fetch(window.location.href + "api/get-coords?loc=" + start)
     .then(response => response.json())
     .then(result => {
         SetCookie("startCoord", result.result, 1);
+        firstCookieDone = true;
+        CheckIfDone();
     }).catch(error => {
         document.getElementById("testGetCookieError").innerHTML = "error set cookie";
-    })
+    });
 
     fetch(window.location.href + "api/get-coords?loc=" + end)
     .then(response => response.json())
     .then(result => {
         SetCookie("endCoord", result.result, 1);
-        window.location.href = "/page2";
+        secondCookieDone = true;
+        CheckIfDone();
     }).catch(error => {
         document.getElementById("testGetCookieError").innerHTML = "error set cookie";
     });
-});
 
+    setTimeout(() => {}, 500);
+}
+
+function CheckIfDone(){
+    window.location.href = "/page2";
+}
 
 //cname = string, cvalue = string, exdays = int
 //cookie will expire after exdays
