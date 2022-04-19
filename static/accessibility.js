8894ae6245446148da72b596b7aafc9cc02b7df9
Zach Robinson <zach.robinson@tamu.edu>
2022-04-19 14:30:51 -0500

    Added text enlargement to accessibility button

--- static/accessibility.js
+++ static/accessibility.js
@@ -1,21 +1,22 @@
 // When accesibility button is clicked
 
 function change() {
-    var theme = document.getElementsByTagName('link')[0];
-    if (theme.getAttribute('href') == 'style.css') {
-        theme.setAttribute('href', 'style2.css');
+    var theme = document.getElementById("theme");
+    if (theme.getAttribute('href') == "../static/style.css") {
+        theme.setAttribute('href', "../static/style2.css");
     } else {
-        theme.setAttribute('href', 'style.css');
+        theme.setAttribute('href', "../static/style.css");
     }
     localStorage.setItem("theme", theme.getAttribute('href'));
 }
 function setDefaultTheme(){
     var defaultTheme = localStorage.getItem("theme");
     if(!defaultTheme){
-        localStorage.setItem("theme", "style.css");
+        localStorage.setItem("theme", "../static/style.css");
     }
+    
 }
 function chooseTheme(){
     setDefaultTheme();
-    document.getElementsByTagName('link')[0].setAttribute('href', localStorage.getItem("theme"));
+    document.getElementById("theme").setAttribute('href', localStorage.getItem("theme"));
 }
